9f27376b2086c8ad9634ec5789de2fd5714c014e
Mohamed-khaled0 <mohamedalshraby3@gmail.com>
2024-10-10 00:30:25 +0300

    Fix the dark and light mode

--- src/components/1-header/Header.jsx
+++ src/components/1-header/Header.jsx
@@ -4,10 +4,18 @@ import "./header.css";
 
 export default function Header() {
   let [showModal, setShowModal] = useState(false);
-  let [theme, setTheme] = useState("dark");
+  const [theme, setTheme] = useState(
+    localStorage.getItem("currentMode") ?? "dark"
+  );
 
   useEffect(() => {
-    document.body.classList.add(theme);
+    if (theme === "light") {
+      document.body.classList.remove("dark");
+      document.body.classList.add("light");
+    } else {
+      document.body.classList.remove("light");
+      document.body.classList.add("dark");
+    }
   }, [theme]);
 
   return (
@@ -51,8 +59,24 @@ export default function Header() {
           </ul>
         </nav>
 
-        <button className="mode flex">
-          <span className="icon-moon-o"></span>
+        <button
+          onClick={() => {
+            // Send value to LS
+            localStorage.setItem(
+              "currentMode",
+              theme === "dark" ? "light" : "dark"
+            );
+
+            // get value from LS
+            setTheme(localStorage.getItem("currentMode"));
+          }}
+          className="mode flex"
+        >
+          {theme === "dark" ? (
+            <span className="icon-moon-o"> </span>
+          ) : (
+            <span className="icon-sun"> </span>
+          )}
         </button>
 
         {showModal && (
