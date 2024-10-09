b7170248e284734af8e830e4128af76ddd40c47e
Mohamed-khaled0 <mohamedalshraby3@gmail.com>
2024-10-09 15:02:12 +0300

    Scrollbar settings

--- src/components/5-footer/Footer.jsx
+++ src/components/5-footer/Footer.jsx
@@ -4,8 +4,16 @@ import './footer.css'
 
 export default function Footer() {
   return (
-    <footer>
-        Footer
+    <footer className='flex'>
+        <ul className='flex'>
+          <li><a href="">About</a></li>
+          <li><a href="">Projects</a></li>
+          <li><a href="">Education</a></li>
+          <li><a href="">Skills</a></li>
+          <li><a href="">Contact</a></li>
+        </ul>
+
+        <p>Created by Mohamed Khaled ,All rights reserved.</p>
     </footer>
   )
 }
