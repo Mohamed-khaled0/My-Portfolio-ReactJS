96a1882eeb182c88d8d2067641df053216366d0e
Mohamed-khaled0 <mohamedalshraby3@gmail.com>
2024-10-10 21:31:48 +0300

    Restructure

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,11 @@ import Hero from './components/2-hero/Hero'
 import Header from './components/1-header/Header'
 import Main from './components/3-main/Main'
 import Contact from './components/4-contact/Contact'
-import Footer from './components/5-footer/Footer'
+import Footer from './components/8-footer/Footer'
 import Scroll2Top from './components/Scroll2Top.jsx';
-import Certifications from './components/certificates/Certifications'
-import Education from './components/education/Education'
-import Skills from './components/skills/Skills'
+import Certifications from './components/7-certificates/Certifications'
+import Education from './components/6-education/Education'
+import Skills from './components/4-skills/Skills'
 
 function App() {
 
