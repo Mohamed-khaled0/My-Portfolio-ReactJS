309400fb2cf26a62ee8c9137715612d76c8d3d95
Mohamed-khaled0 <mohamedalshraby3@gmail.com>
2024-10-10 16:43:00 +0300

    Change the main color

--- src/components/2-hero/Hero.jsx
+++ src/components/2-hero/Hero.jsx
@@ -8,43 +8,43 @@ export default function Hero() {
   let lottieRef = useRef();
   return (
     <section id="main" className="hero flex">
-      <div className="left-section ">
+      <div className="left-section flex">
         <div className="parent-avatar flex">
           <img
             className="avatar"
             src="./myimg.jpg"
             alt=""
           />
+          <h1 className="title">Hi 👋🏻 I’m Mohamed Khaled</h1>
+          <h1 className="title">
+            Software engineer mainly focused on Web Development
+          </h1>
+
+          <p className="sub-title">
+            I'm currently interning as a web developer at the Information Technology
+            Institute (ITI). I spend my day working on projects with HTML, CSS,
+            Tailwind, JavaScript, TypeScript, and React.js.
+          </p>
         </div>
 
-        <h1 className="title">Hi 👋🏻 I’m Mohamed Khaled </h1>
-        <h1 className="title">
-          Software engineer mainly focused on Web Development{" "}
-        </h1>
-
-        <p className="sub-title">
-          I'm currently intern as a web developer at Information Technology
-          Institute ITI. I spend my day working on projects with HTML , CSS ,
-          Tailwind , JavaScript ,TypeScript , React.js.
-        </p>
-        <a href="#" target="_blank">
-          <button>
-            <span className="icon-cloud-download"></span> Resume
-          </button>
-        </a>
-        <a href="https://github.com/Mohamed-khaled0" target="_blank">
-          <button>
-            <span className="icon-github"></span> Github
-          </button>
-        </a>
-
-        <a href="https://www.linkedin.com/in/mohamed-khaled3/" target="_blank">
-          <button>
-            <span className="icon-linkedin"></span> LinkedIn
-          </button>
-        </a>
+        <div className="button-group flex">
+          <a href="#" target="_blank">
+            <button>
+              <span className="icon-cloud-download"></span> Resume
+            </button>
+          </a>
+          <a href="https://github.com/Mohamed-khaled0" target="_blank">
+            <button>
+              <span className="icon-github"></span> Github
+            </button>
+          </a>
+          <a href="https://www.linkedin.com/in/mohamed-khaled3/" target="_blank">
+            <button>
+              <span className="icon-linkedin"></span> LinkedIn
+            </button>
+          </a>
+        </div>
       </div>
-
     </section>
   );
 }
