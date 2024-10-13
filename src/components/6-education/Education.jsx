c20d07195c653a3a08396f19543c805c0dc061e1
Mohamed-khaled0 <mohamedalshraby3@gmail.com>
2024-10-13 22:31:15 +0300

    Add localization for education section

--- src/components/6-education/Education.jsx
+++ src/components/6-education/Education.jsx
@@ -1,39 +1,32 @@
 /* eslint-disable no-unused-vars */
 import React from 'react';
-import {  FaUniversity } from 'react-icons/fa';
+import { FaUniversity } from 'react-icons/fa';
+import { useTranslation } from 'react-i18next';
 import './education.css';
 
 const Education = () => {
-    const educationData = {
-        title: 'Bachelor of Computer Science',
-        institution: 'Thebes Higher Institute of Information Technology',
-        yearRange: '2021 : 2025',
-        gpa: 'GPA 3/4',
-        major: '- Major in Computer Science',
-        minor: '- Minor in Web Applications',
-        imageUrl: "https://i.imgur.com/wSab0Hn.jpegnpm "
-    };
+  const { t } = useTranslation('education'); // Use the 'education' namespace
 
-    return (
-        <section id="education">
-            <h2 id='title'>Education </h2>
-            <div className="education-card">
-                <div className="education-card-left">
-                    <img src={educationData.imageUrl} alt="Thebes Academy Logo" />
-                </div>
-                <div className="education-card-right">
-                    <h3><FaUniversity /> {educationData.title}</h3>
-                    <p>{educationData.institution}</p>
-                    <p>{educationData.yearRange}</p>
-                    <p>{educationData.gpa}</p>
-                    <ul>
-                        <li>  {educationData.major}</li>
-                        <li> {educationData.minor}</li>
-                    </ul>
-                </div>
-            </div>
-        </section>
-    );
+  return (
+    <section id="education">
+      <h2 id='title'>{t('title')}</h2>
+      <div className="education-card">
+        <div className="education-card-left">
+          <img src="https://i.imgur.com/wSab0Hn.jpegnpm" alt="Thebes Academy Logo" />
+        </div>
+        <div className="education-card-right">
+          <h3><FaUniversity /> {t('title')}</h3>
+          <p>{t('institution')}</p>
+          <p>{t('yearRange')}</p>
+          <p>{t('gpa')}</p>
+          <ul>
+            <li>{t('major')}</li>
+            <li>{t('minor')}</li>
+          </ul>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Education;
