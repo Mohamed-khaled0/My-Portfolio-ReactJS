bdfeeff46c03781dc2e854710b8ecd54b46a13fe
Mohamed-khaled0 <mohamedalshraby3@gmail.com>
2025-06-19 01:44:35 +0300

    Add new skills to the Skills component, including Python, CCNA, and Microsoft technologies, while removing outdated entries for Flutter and Dart.

--- src/components/4-skills/Skills.jsx
+++ src/components/4-skills/Skills.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable no-unused-vars */
 import React from 'react';
 import { motion } from 'framer-motion';
-import { SiTailwindcss, SiTypescript,  SiPostman, SiFlutter, SiDart } from 'react-icons/si';
+import { SiTailwindcss, SiTypescript, SiPostman, SiFlutter, SiDart, SiPython } from 'react-icons/si';
 import { 
   FaHtml5, 
   FaCss3Alt, 
@@ -10,25 +10,30 @@ import {
   FaGit, 
   FaGithub, 
   FaDatabase, 
+  FaNetworkWired,
+  FaWindows,
+  FaServer,
 } from 'react-icons/fa';
 import { useTranslation } from 'react-i18next'; // Import useTranslation
 import './skills.css';
 
 
 const skillsData = [
-  { name: 'Flutter', icon: <SiFlutter size={50} style={{ color: '#02569B' }} /> }, 
-  { name: 'Dart', icon: <SiDart size={50} style={{ color: '#00BFFF' }} /> }, 
   { name: 'HTML', icon: <FaHtml5 size={50} style={{ color: '#E44D26' }} /> },
   { name: 'CSS', icon: <FaCss3Alt size={50} style={{ color: '#1572B6' }} /> },
   { name: 'Tailwind', icon: <SiTailwindcss size={50} style={{ color: '#38B2AC' }} /> },
   { name: 'JavaScript', icon: <FaJs size={50} style={{ color: '#F7DF1E' }} /> },
-  { name: 'TypeScript', icon: <SiTypescript size={50} style={{ color: '#007ACC' }} /> },
   { name: 'React', icon: <FaReact size={50} style={{ color: '#61DAFB' }} /> },
   { name: 'MySQL', icon: <FaDatabase size={50} style={{ color: '#4479A1' }} /> },
   { name: 'Postman', icon: <SiPostman size={50} style={{ color: '#FF6C37' }} /> },
   { name: 'Git', icon: <FaGit size={50} style={{ color: '#F05032' }} /> },
   { name: 'GitHub', icon: <FaGithub size={50} style={{ color: '#181717' }} /> },
-
+  { name: 'CCNA', icon: <FaNetworkWired size={50} style={{ color: '#0077b6' }} /> },
+  { name: 'Cisco Packet Tracer', icon: <FaNetworkWired size={50} style={{ color: '#00BFFF' }} /> },
+  { name: 'Python', icon: <SiPython size={50} style={{ color: '#3776AB' }} /> },
+  { name: 'SQL', icon: <FaDatabase size={50} style={{ color: '#4DB33D' }} /> },
+  { name: 'Microsoft Office', icon: <FaWindows size={50} style={{ color: '#ea3e23' }} /> },
+  { name: 'Microsoft Server', icon: <FaServer size={50} style={{ color: '#0077b6' }} /> },
 ];
 const Skills = () => {
   const { t } = useTranslation('skills'); // Specify 'skills' namespace
