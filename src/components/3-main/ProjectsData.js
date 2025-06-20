8e46ec27b73a7ed34a8aaac37e7c717d5f694390
Mohamed-khaled0 <mohamedalshraby3@gmail.com>
2025-06-20 12:39:07 +0300

    Update localization files for E-Commerce project in English and German, adding detailed descriptions, subtitles, and features. Introduce new Price Tracker project with relevant information in ProjectsData.js, enhancing project portfolio visibility.

--- src/components/3-main/ProjectsData.js
+++ src/components/3-main/ProjectsData.js
@@ -13,6 +13,17 @@ let myProjects = [
     technologies: ["ReactJS", "HTML5", "CSS3", "Framer Motion", "Vite", "Netlify"]
   },
   {
+    projectTitle: "ecommerce_project_title",
+    projectSubtitle: "ecommerce_project_subtitle",
+    description: "ecommerce_project_description",
+    category: ["react", "all"],
+    liveDemo: "https://prisetracker.netlify.app/",
+    githubLink: "https://github.com/Mohamed-khaled0/Hardware-Price-Tracker",
+    imgPath: "/images/pricetracker.webp",
+    features: ["ecommerce_project_features"],
+    technologies: ["ReactJS", "HTML5", "CSS3", "JavaScript", "Netlify"]
+  },
+  {
     projectTitle: "crypto_project_title",
     projectSubtitle: "crypto_project_subtitle",
     description: "crypto_project_description",
